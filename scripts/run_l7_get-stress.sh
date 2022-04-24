8d284a0c1fa229ab9225b0c27aa8c3cb14119e79
Flux State <f1ux.state@pm.me>
2022-04-24 23:12:39 +0200

    targets cleanup and update

--- scripts/run_l7_get-stress.sh
+++ scripts/run_l7_get-stress.sh
@@ -4,5 +4,5 @@ python3 runner.py \
   --config https://raw.githubusercontent.com/FluxState/warlists/main/l7_get-stress.lst \
   --debug \
   --http-methods GET STRESS \
-  --threads 16 \
+  --threads 12 \
   --vpn 90
