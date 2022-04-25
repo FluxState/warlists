f4dcf2115ab2e621a88c7f31007b1c54b3b18abf
Flux State <f1ux.state@pm.me>
2022-04-26 00:29:32 +0200

    targets cleanup and update

--- scripts/run_l7.sh
+++ scripts/run_l7.sh
@@ -4,5 +4,5 @@ python3 runner.py \
   --config https://raw.githubusercontent.com/FluxState/warlists/main/l7.lst \
   --debug \
   --http-methods GSB \
-  --threads 494 \
+  --threads 332 \
   --vpn 90
--- scripts/run_l7_cf.sh
+++ scripts/run_l7_cf.sh
@@ -3,5 +3,5 @@
 python3 runner.py \
   --config https://raw.githubusercontent.com/FluxState/warlists/main/l7_cf.lst \
   --debug \
-  --http-methods CFB GSB \
-  --threads 40
+  --http-methods CFB CFBUAM GSB \
+  --threads 156
