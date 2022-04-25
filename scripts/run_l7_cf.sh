2030b67e2bb64ee0fea031c78fc407d86c5c7ce0
Flux State <f1ux.state@pm.me>
2022-04-26 00:48:42 +0200

    targets cleanup

--- scripts/run_l7_cf.sh
+++ scripts/run_l7_cf.sh
@@ -3,5 +3,5 @@
 python3 runner.py \
   --config https://raw.githubusercontent.com/FluxState/warlists/main/l7_cf.lst \
   --debug \
-  --http-methods CFB CFBUAM GSB \
-  --threads 156
+  --http-methods CFB GSB \
+  --threads 96
