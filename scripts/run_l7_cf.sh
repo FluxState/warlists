7e2bdda717246e504be7931b9f0566fae2668a85
FluxState <98568457+FluxState@users.noreply.github.com>
2022-04-28 00:13:43 +0200

    Update run_l7_cf.sh

--- scripts/run_l7_cf.sh
+++ scripts/run_l7_cf.sh
@@ -6,5 +6,5 @@ python3 runner.py \
   --config https://raw.githubusercontent.com/FluxState/warlists/main/l7_cf.lst \
   --debug \
   --http-methods CFB GSB \
-  --threads 96 \
+  --threads 56 \
   --vpn 100
