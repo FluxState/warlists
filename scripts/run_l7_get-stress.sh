79caecf39a53746d4735219b273cd0ae2710fe37
FluxState <98568457+FluxState@users.noreply.github.com>
2022-04-29 01:23:08 +0200

    Update run_l7_get-stress.sh

--- scripts/run_l7_get-stress.sh
+++ scripts/run_l7_get-stress.sh
@@ -6,5 +6,5 @@ python3 runner.py \
   --config https://raw.githubusercontent.com/FluxState/warlists/main/l7_get-stress.lst \
   --debug \
   --http-methods GET STRESS \
-  --threads 4 \
-  --vpn 100
+  --threads 8 \
+  --vpn
