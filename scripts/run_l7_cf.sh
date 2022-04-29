e6d3ccb85e1f379c631d26924c79767a340d7319
Flux State <f1ux.state@pm.me>
2022-04-29 02:57:36 +0200

    typo fix

--- scripts/run_l7_cf.sh
+++ scripts/run_l7_cf.sh
@@ -5,6 +5,6 @@
 python3 runner.py \
   --config https://raw.githubusercontent.com/FluxState/warlists/main/l7_cf.lst \
   --debug \
-  --http-methods CFB CDBUAM GSB \
+  --http-methods CFB CFBUAM GSB \
   --threads 180 \
   --vpn 90
