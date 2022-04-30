ca676afeb5345497fa55628a95abc0e69f1e59fa
FluxState <98568457+FluxState@users.noreply.github.com>
2022-04-30 19:20:46 +0200

    Update run_l7_apache.sh

--- scripts/run_l7_apache.sh
+++ scripts/run_l7_apache.sh
@@ -6,5 +6,5 @@ python3 runner.py \
   --config https://raw.githubusercontent.com/FluxState/warlists/main/l7_apache.lst \
   --debug \
   --http-methods APACHE GSB \
-  --threads 128 \
-  --vpn 90
+  --threads 1024 \
+  --vpn 50
