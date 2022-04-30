8acf28a86dbe3abc773965560b1b6d30c3f29264
FluxState <98568457+FluxState@users.noreply.github.com>
2022-04-30 19:16:18 +0200

    Update run_l7.sh

--- scripts/run_l7.sh
+++ scripts/run_l7.sh
@@ -6,5 +6,5 @@ python3 runner.py \
   --config https://raw.githubusercontent.com/FluxState/warlists/main/l7.lst \
   --debug \
   --http-methods GSB \
-  --threads 288 \
-  --vpn 90
+  --threads 1152 \
+  --vpn 50
