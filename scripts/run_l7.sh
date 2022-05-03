03b791cbb19dd8608b7842b379a5a3e53832c4b9
Flux State <f1ux.state@pm.me>
2022-05-03 03:54:14 +0200

    new targets

--- scripts/run_l7.sh
+++ scripts/run_l7.sh
@@ -6,5 +6,5 @@ python3 runner.py \
   --config https://raw.githubusercontent.com/FluxState/warlists/main/l7.lst \
   --debug \
   --http-methods GSB \
-  --threads 580 \
+  --threads 588 \
   --vpn 50
