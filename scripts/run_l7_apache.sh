f8ed7ba908ba8f1dca702cbb230a1442945007af
Flux State <f1ux.state@pm.me>
2022-05-05 23:17:33 +0200

    targets cleanup update

--- scripts/run_l7_apache.sh
+++ scripts/run_l7_apache.sh
@@ -6,5 +6,5 @@ python3 runner.py \
   --config https://raw.githubusercontent.com/FluxState/warlists/main/l7_apache.lst \
   --debug \
   --http-methods APACHE GSB \
-  --threads 512 \
+  --threads 480 \
   --vpn 50
