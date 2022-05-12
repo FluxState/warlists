138fbc7b0ec2ffdd7a71bbf4ceb4a7ab5732455b
Flux State <f1ux.state@pm.me>
2022-05-12 09:31:57 +0200

    targets update

--- scripts/run_l7_apache.sh
+++ scripts/run_l7_apache.sh
@@ -6,5 +6,5 @@ python3 runner.py \
   --config https://raw.githubusercontent.com/FluxState/warlists/main/l7_apache.lst \
   --debug \
   --http-methods APACHE GSB \
-  --threads 224 \
+  --threads 208 \
   --vpn 50
