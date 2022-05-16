fa7c59ebfac154664e5fa8adb2d4d11f82166666
Flux State <f1ux.state@pm.me>
2022-05-16 05:12:42 +0200

    drop apache method

--- scripts/run_l7.sh
+++ scripts/run_l7.sh
@@ -6,5 +6,5 @@ python3 runner.py \
   --config https://raw.githubusercontent.com/FluxState/warlists/main/l7.lst \
   --debug \
   --http-methods GSB \
-  --threads 1200 \
+  --threads 1304 \
   --vpn
