cfb7034df1b380290473709a5ab14e2c71afec39
Flux State <f1ux.state@pm.me>
2022-05-17 12:49:50 +0200

    scripts update (new mhd)

--- scripts/run_l7.sh
+++ scripts/run_l7.sh
@@ -5,6 +5,6 @@
 python3 runner.py \
   --config https://raw.githubusercontent.com/FluxState/warlists/main/l7.lst \
   --debug \
-  --http-methods GSB \
+  --http-methods RGET \
   --threads 2608 \
   --vpn
--- scripts/run_l7_cf.sh
+++ scripts/run_l7_cf.sh
@@ -5,6 +5,6 @@
 python3 runner.py \
   --config https://raw.githubusercontent.com/FluxState/warlists/main/l7_cf.lst \
   --debug \
-  --http-methods CFB GSB \
+  --http-methods CFB RGET \
   --threads 224 \
   --vpn
