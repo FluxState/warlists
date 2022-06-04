396b7fa7451e9c065e103c242f66f4bac4edcf47
Flux State <f1ux.state@pm.me>
2022-06-04 17:16:14 +0200

    targets update

--- scripts/run_l7.sh
+++ scripts/run_l7.sh
@@ -7,5 +7,5 @@ python3 runner.py \
   --debug \
   --http-methods RGET \
   --lang en \
-  --threads 2400 \
+  --threads 2448 \
   --vpn
--- scripts/run_l7_get-stress.sh
+++ scripts/run_l7_get-stress.sh
@@ -7,5 +7,5 @@ python3 runner.py \
   --debug \
   --http-methods GET STRESS \
   --lang en \
-  --threads 688 \
+  --threads 720 \
   --vpn
