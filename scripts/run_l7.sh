1fa66d7d05374bad1edaf195634f1df2de435d6f
Flux State <f1ux.state@pm.me>
2022-07-27 11:39:11 +0200

    scripts update

--- scripts/run_l7.sh
+++ scripts/run_l7.sh
@@ -2,17 +2,22 @@
 
 # https://github.com/porthole-ascend-cinnamon/mhddos_proxy
 
-calc() { awk "BEGIN{ printf \"%.2f\n\", $* }"; }
-
 BASE_DIR="$(dirname -- "$(readlink -f "${BASH_SOURCE}")")"
 BASE_FILE='l7.lst'
 CURRENT_DAY=$(printf %02d `date +%d`)
 DAYS_IN_MONTH=$(cal |egrep -v [a-z] |wc -w)
 LINES=$(cat $BASE_DIR/../$BASE_FILE | wc -l)
-LINES_PER_PART=$(echo "$(calc "$LINES/$DAYS_IN_MONTH")" | python3 -c "print(round(float(input())))")
+LINES_PER_PART=$(python3 -c "print(round(float($LINES/$DAYS_IN_MONTH)))")
 TARGETS_FILE="$BASE_DIR/../$BASE_FILE"
-TARGETS_FILE_SPLIT="$BASE_DIR/../$BASE_FILE.$CURRENT_DAY"
 ((THREADS=$LINES*(2**5)))
+
+MAX_SPLIT_N=$(python3 -c "from math import ceil; print(ceil(float($LINES/$LINES_PER_PART)))")
+if [ "$DAYS_IN_MONTH" -gt "$MAX_SPLIT_N" ]; then
+  CURRENT_SPLIT=$MAX_SPLIT_N
+else
+  CURRENT_SPLIT=$CURRENT_DAY
+fi
+TARGETS_FILE_SPLIT="$BASE_DIR/../$BASE_FILE.$CURRENT_SPLIT"
 
 split --numeric-suffixes=1 -l $LINES_PER_PART $TARGETS_FILE "$TARGETS_FILE."
 
