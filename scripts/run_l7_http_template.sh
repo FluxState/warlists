72ab893c06b48a351c458f9c4a56864b168efec2
Flux State <f1ux.state@pm.me>
2022-10-05 17:39:24 +0200

    http_template init

---
+++ scripts/run_l7_http_template.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# https://github.com/porthole-ascend-cinnamon/mhddos_proxy
+
+BASE_DIR="$(dirname -- "$(readlink -f "${BASH_SOURCE}")")"
+CURRENT_DATE=$(date +%Y%m%d)
+TARGETS_FILE="$BASE_DIR/../http_template/list.$CURRENT_DATE.json"
+
+if [ -r "$TARGETS_FILE" ]; then
+  LINES=$(cat $TARGETS_FILE | wc -l)
+  ((THREADS=$LINES*(2**5)))
+
+  python3 runner.py \
+    --config "$TARGETS_FILE" \
+    --lang en \
+    --threads "$THREADS" \
+    --vpn
+fi
